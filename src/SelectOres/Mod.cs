bf6b5920acec7b95a0d3fd3e0487f79985af152d
neverify <203544938+neverify@users.noreply.github.com>
2026-08-14 22:38:06 +0300

    refactor: switch to harmony annotations

--- src/SelectOres/Mod.cs
+++ src/SelectOres/Mod.cs
@@ -1,9 +1,5 @@
-using HarmonyLib;
-using SelectOres.Patches;
-using Terraria;
 using TerrariaModder.Core;
 using TerrariaModder.Core.Logging;
-using Utils;
 
 namespace SelectOres;
 
@@ -15,40 +11,12 @@ public class Mod : IMod
 
     internal static ILogger Log;
     internal static Config Config;
-    private static Harmony s_harmony;
 
     public void Initialize(ModContext context)
     {
         Log = context.Logger;
         Config = context.GetConfig<Config>();
-        s_harmony = new Harmony("com.neverify.select-ores");
     }
 
-    public static void OnGameReady()
-    {
-        if (s_harmony == null)
-            return;
-
-        var patcher = new Patcher(s_harmony, Log);
-
-        patcher.Patch(
-            typeof(WorldGen),
-            "Reset",
-            prefix: Patcher.GetHarmonyMethod(
-                typeof(SmashAltarPatch),
-                nameof(SmashAltarPatch.Prefix)
-            ),
-            postfix: Patcher.GetHarmonyMethod(typeof(ResetPatch), nameof(ResetPatch.Postfix))
-        );
-    }
-
-    public void OnWorldUnload() { }
-
-    public void OnConfigChanged() { }
-
-    public void Unload()
-    {
-        s_harmony.UnpatchAll("com.neverify.select-ores");
-        Log.Info("Select Ores unloaded.");
-    }
+    public void Unload() { }
 }
--- src/SelectOres/Patches/WorldGen_Reset.cs
+++ src/SelectOres/Patches/WorldGen_Reset.cs
@@ -1,12 +1,24 @@
 using System.Collections.Generic;
+using HarmonyLib;
 using Terraria;
 using Terraria.ID;
 using Terraria.WorldBuilding;
 
 namespace SelectOres.Patches;
 
+[HarmonyPatch(typeof(WorldGen), nameof(WorldGen.Reset))]
 internal sealed class ResetPatch
 {
+    internal static void Prefix()
+    {
+        if (!Mod.Config.OverrideGeneration)
+            return;
+
+        SetOreTier(ref WorldGen.SavedOreTiers.Cobalt, Mod.Config.HardmodeTier1Ore);
+        SetOreTier(ref WorldGen.SavedOreTiers.Mythril, Mod.Config.HardmodeTier2Ore);
+        SetOreTier(ref WorldGen.SavedOreTiers.Adamantite, Mod.Config.HardmodeTier3Ore);
+    }
+
     internal static void Postfix()
     {
         if (!Mod.Config.OverrideGeneration)
@@ -28,6 +40,12 @@ internal sealed class ResetPatch
         WorldGen.SavedOreTiers.Gold = s_oreNameToTileId[Mod.Config.Tier4Ore];
     }
 
+    private static void SetOreTier(ref int savedTier, string oreName)
+    {
+        if (savedTier == -1)
+            savedTier = s_oreNameToTileId[oreName];
+    }
+
     private static readonly Dictionary<string, int> s_oreNameToTileId = new()
     {
         { "Copper", TileID.Copper },
@@ -38,6 +56,12 @@ internal sealed class ResetPatch
         { "Lead", TileID.Lead },
         { "Tungsten", TileID.Tungsten },
         { "Platinum", TileID.Platinum },
+        { "Cobalt", TileID.Cobalt },
+        { "Mythril", TileID.Mythril },
+        { "Adamantite", TileID.Adamantite },
+        { "Palladium", TileID.Palladium },
+        { "Orichalcum", TileID.Orichalcum },
+        { "Titanium", TileID.Titanium },
     };
 
     private static readonly Dictionary<string, int> s_oreNameToBarId = new()
