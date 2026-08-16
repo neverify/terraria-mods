df39539d00f8eb81ed9d9409a426dacd0e1f4f3b
neverify <203544938+neverify@users.noreply.github.com>
2026-08-16 19:50:54 +0300

    fix: add `null` guard to `OnGameReady()`

--- src/FullBright/Mod.cs
+++ src/FullBright/Mod.cs
@@ -9,7 +9,13 @@ public class Mod : ModBase<Mod, Config>
     public override string Name => "Fullbright";
     public override string Version => "1.1.1";
 
-    public static void OnGameReady() => LightingQuality.SetQuality();
+    public static void OnGameReady()
+    {
+        if (Instance is null)
+            return;
+
+        LightingQuality.SetQuality();
+    }
 
     public void OnConfigChanged() => LightingQuality.SetQuality();
 }
