50d69b79ca9d7449fb2ca59214331c5e6be5c1ed
neverify <203544938+neverify@users.noreply.github.com>
2026-08-23 15:41:59 +0300

    feat: don't remove tooltip if the mod is disabled

--- src/FunctionalSocialAccessories/Patches/Main_MouseText_DrawItemTooltip_GetLinesInfo.cs
+++ src/FunctionalSocialAccessories/Patches/Main_MouseText_DrawItemTooltip_GetLinesInfo.cs
@@ -10,9 +10,21 @@ internal static class MouseText_DrawItemTooltip_GetLinesInfoPatch
 
     private static void Prefix(Item item, out bool __state)
     {
+        if (!Mod.Instance.Config.FunctionalSocialSlots)
+        {
+            __state = false;
+            return;
+        }
+
         __state = item.social;
         item.social = false;
     }
 
-    private static void Postfix(Item item, bool __state) => item.social = __state;
+    private static void Postfix(Item item, bool __state)
+    {
+        if (!Mod.Instance.Config.FunctionalSocialSlots)
+            return;
+
+        item.social = __state;
+    }
 }
