5870063fae9c6982ebb7392e0d9ccb682df60f9a
neverify <203544938+neverify@users.noreply.github.com>
2026-08-23 17:01:15 +0300

    feat: initial commit

---
+++ src/Undeprecate/Config.cs
@@ -0,0 +1,8 @@
+using TerrariaModder.Core.Config;
+
+namespace Undeprecate;
+
+public class Config : ModConfig
+{
+    public override int Version => 1;
+}
---
+++ src/Undeprecate/Mod.cs
@@ -0,0 +1,22 @@
+using System;
+using Terraria.ID;
+using Utils;
+
+namespace Undeprecate;
+
+public class Mod : ModBase<Mod, Config>
+{
+    public override string Id => "undeprecate";
+    public override string Name => "Undeprecate";
+    public override string Version => "1.0.0";
+
+    protected override void Initialize()
+    {
+        Array.Clear(ItemID.Sets.Deprecated, 0, ItemID.Sets.Deprecated.Length);
+        Array.Clear(
+            ItemID.Sets.ItemsThatShouldNotBeInInventory,
+            0,
+            ItemID.Sets.ItemsThatShouldNotBeInInventory.Length
+        );
+    }
+}
